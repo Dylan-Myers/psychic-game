f7ecd1881577c80786766ebe33ac837605d5ac6a
Dylan Myers <dylanmyers178@gmail.com>
2019-12-15 16:18:22 -0600

    does not work

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft= 9;
+var yourGuessesSoFar = [];
+var ranLetter = ranLetter;
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+
+var ranLetter = math.floor(math.random() * letters.length);
+var computerChoice = letters[randomIndex];
+
+console.log(computerChoice);
+
+document.onkeyup = function(event) {
+    var userChoice = event.key;
+    var regexp = /[a-z]/gi;
+   
+      if (guessesLeft <= 0) {
+        lossCount++;
+        document.getElementById("losses").innerHTML = lossCount++;
+        guessesRemaining = 9;
+        guessedLetters = [];
+        document.getElementById("YourGuessesSoFar").innerHTML = guessedLetters;
+        document.getElementById("GuessesLeft").innerHTML = 9;
+        randomIndex = Math.floor(Math.random() * alphabet.length);
+        computerChoice = alphabet[randomIndex];
+      }
+    
+      if (computerChoice === userChoice) {
+        document.getElementById("wins").innerHTML = wins++;
+        guessedLetters = [];
+        document.getElementById("yourGuessesSoFar").innerHTML = yourGuessesSoFar;
+        randomIndex = Math.floor(Math.random() * letters.length);
+        computerChoice = letters[randomIndex];
+        guessesRemaining = 9;
+        document.getElementById("guessesLeft").innerHTML = 9;
+      } else {
+        document.getElementById("guessesLeft").innerHTML = guessesRemaining;
+        guessedLetters.push(userChoice);
+        document.getElementById("guessesSoFar").innerHTML = guessedLetters;
+      }
+//this is not working and I dont know why. I have looked at so many examples online. 
