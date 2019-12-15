c5a947b4e8b0c8f9410252d0b4ac8e35c1d13dc6
Dylan Myers <dylanmyers178@gmail.com>
2019-12-15 17:58:26 -0600

    tried to use different ideas still wont work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,51 @@
 var wins = 0;
 var losses = 0;
-var yourGuessesLeft= 9;
-var yourGuessesSoFar = [];
+var guessesLeft= 9;
+var guessesSoFar = [];
+var ranLetter = ranLetter;
+var letters = "abcdefghijklmnopqrstuvwxyz";
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var ranLetter = letters[Math.floor(Math.random() * letters.length)];
+
+console.log(ranLetter);
+
+function pcGuess() {
+    ranLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(ranLetter);
+}
 
 
-var ranLetter = math.floor(math.random()= letters.length);
-var computerChoice = letters[randomIndex];
-
-console.log(computerChoice);
-
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
     var userChoice = event.key;
-    var regexp = /[a-z]/gi;
-      if (!regexp.test(userChoice)) {
-      }
     
-   
-      if (yourGuessesLeft <= 0) {
-        lossCount++;
-        document.getElementById("losses").innerHTML = lossCount++;
-        guessesRemaining = 9;
-        guessedLetters = [];
-        document.getElementById("guessesSoFar").innerHTML = guessedLetters;
-        document.getElementById("guessesLeft").innerHTML = 9;
-        randomLetter = Math.floor(Math.random() * alphabet.length);
-        computerChoice = alphabet[randomIndex];
-      }
-    
-      if (computerChoice === userChoice) {
-        document.getElementById("win").innerHTML = wins++;
-        guessedLetters = [];
-        document.getElementById("guessesSoFar").innerHTML = yourGuessesSoFar;
-        randomLetter = Math.floor(Math.random() * letters.length);
-        computerChoice = letters[randomIndex];
-        guessesRemaining = 9;
-        document.getElementById("guessesLeft").innerHTML = 9;
-      } 
-      else {
-        document.getElementById("guessesLeft").innerHTML = yourGuessesLeft;
-        guessedLetters.push(userChoice);
-        document.getElementById("guessesSoFar").innerHTML = yourGuessesSoFar;
-      }
-    }   
-//this is not working and I dont know why. I have looked at so many examples online. 
+    if (userChoice === ranLetter) {
+        wins++;
+        guessesLeft = 9;
+        guessesSoFar = [];
+    }
+
+    if (userChoice !== ranletter) {
+        guessesLeft--;
+    }
+
+    if (guessesLeft === 0) {
+        losses++;
+        guessesSoFar = [];
+        guessesLeft = 9;
+    }
+
+    if (guessesSoFar.indexOf(userChoice) >= 0) {
+
+
+
+    } else {
+        guessesSoFar.push(guessesLeft)
+        document.getElementById("guessSoFar").innerHTML = guessesLeft;
+        console.log(guessesSoFar);
+    }
+
+    document.getElementById("win").innerHTML = win;
+    document.getElementById("losses").innerHTML = losses;
+    document.getElementById("yourGuessesLeft").innerHTML = guessesLeft;
+} 
