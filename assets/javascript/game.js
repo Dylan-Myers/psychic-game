c7a69ce316e5442c26cac7e7deea030015ef5554
Dylan Myers <dylanmyers178@gmail.com>
2019-12-15 16:44:34 -0600

    Still does not work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 var wins = 0;
 var losses = 0;
-var guessesLeft= 9;
+var yourGuessesLeft= 9;
 var yourGuessesSoFar = [];
-var ranLetter = ranLetter;
+
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 
-var ranLetter = math.floor(math.random() * letters.length);
+var ranLetter = math.floor(math.random()= letters.length);
 var computerChoice = letters[randomIndex];
 
 console.log(computerChoice);
@@ -14,29 +14,34 @@ console.log(computerChoice);
 document.onkeyup = function(event) {
     var userChoice = event.key;
     var regexp = /[a-z]/gi;
+      if (!regexp.test(userChoice)) {
+      }
+    
    
-      if (guessesLeft <= 0) {
+      if (yourGuessesLeft <= 0) {
         lossCount++;
         document.getElementById("losses").innerHTML = lossCount++;
         guessesRemaining = 9;
         guessedLetters = [];
-        document.getElementById("YourGuessesSoFar").innerHTML = guessedLetters;
-        document.getElementById("GuessesLeft").innerHTML = 9;
-        randomIndex = Math.floor(Math.random() * alphabet.length);
+        document.getElementById("guessesSoFar").innerHTML = guessedLetters;
+        document.getElementById("guessesLeft").innerHTML = 9;
+        randomLetter = Math.floor(Math.random() * alphabet.length);
         computerChoice = alphabet[randomIndex];
       }
     
       if (computerChoice === userChoice) {
-        document.getElementById("wins").innerHTML = wins++;
+        document.getElementById("win").innerHTML = wins++;
         guessedLetters = [];
-        document.getElementById("yourGuessesSoFar").innerHTML = yourGuessesSoFar;
-        randomIndex = Math.floor(Math.random() * letters.length);
+        document.getElementById("guessesSoFar").innerHTML = yourGuessesSoFar;
+        randomLetter = Math.floor(Math.random() * letters.length);
         computerChoice = letters[randomIndex];
         guessesRemaining = 9;
         document.getElementById("guessesLeft").innerHTML = 9;
-      } else {
-        document.getElementById("guessesLeft").innerHTML = guessesRemaining;
+      } 
+      else {
+        document.getElementById("guessesLeft").innerHTML = yourGuessesLeft;
         guessedLetters.push(userChoice);
-        document.getElementById("guessesSoFar").innerHTML = guessedLetters;
+        document.getElementById("guessesSoFar").innerHTML = yourGuessesSoFar;
       }
+    }   
 //this is not working and I dont know why. I have looked at so many examples online. 
